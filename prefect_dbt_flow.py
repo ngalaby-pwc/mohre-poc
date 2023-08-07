2e83bc02f39cdb8efab2f96f27ae7489e18e44a7
Nour Galaby <nour.galaby@pwc.com>
2023-08-07 17:33:32 +0300

    prefact start

---
+++ prefect_dbt_flow.py
@@ -0,0 +1,15 @@
+from prefect import Flow, task
+import subprocess
+
+@task
+def run_dbt():
+    # Replace 'dbt run' with the appropriate dbt command for your environment
+    subprocess.run(['dbt', 'run'])
+
+# Create Prefect Flow
+with Flow('dbt_run_flow') as flow:
+    dbt_task = run_dbt()
+
+
+# Run the Prefect Flow
+flow.run()
