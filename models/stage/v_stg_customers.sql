d6bedd4285cdd56205652ce685af061519df5e7e
Ubuntu <azureuser@mohr-poc-vm.zzpzhxbpjtbepc4qsganlxl0gd.bx.internal.cloudapp.net>
2023-08-07 06:23:54 +0000

    merging

---
+++ models/stage/v_stg_customers.sql
@@ -0,0 +1,28 @@
+{{ config(
+    tags=["stage"]
+) }}
+
+{%- set yaml_metadata -%}
+source_model: 'raw_customers'
+derived_columns:
+  RECORD_SOURCE: '!RAW_CUSTOMERS'
+  LOAD_DATE: INSERTION_DATE + INTERVAL '1 day'
+  EFFECTIVE_FROM: 'INSERTION_DATE'
+hashed_columns:
+  TRANSACTION_PK:
+    - 'CUSTOMER_ID'
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{% set source_model = metadata_dict['source_model'] %}
+
+{% set derived_columns = metadata_dict['derived_columns'] %}
+
+{% set hashed_columns = metadata_dict['hashed_columns'] %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=source_model,
+                     derived_columns=derived_columns,
+                     hashed_columns=hashed_columns,
+                     ranked_columns=none) }}
