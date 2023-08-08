a868761545fadf22d218bc18356d365c50a51709
Nour Galaby <nour.galaby@pwc.com>
2023-08-08 14:54:35 +0300

    update prefect_dbt

--- prefect_flows/prefect_dbt_flow.py
+++ prefect_flows/prefect_dbt_flow.py
@@ -1,19 +1,13 @@
 from prefect import flow
-from prefect_dbt.cli import DbtCliProfile, DbtCoreOperation
+from prefect_dbt.cli.commands import DbtCoreOperation
 
 @flow
-def trigger_dbt_flow():
-    dbt_cli_profile = DbtCliProfile.load("DBT-CORE-OPERATION-BLOCK-NAME-PLACEHOLDER")
-    with DbtCoreOperation(
-        commands=["dbt debug", "dbt run"],
+def trigger_dbt_flow() -> str:
+    result = DbtCoreOperation(
+        commands=["pwd", "dbt debug", "dbt run"],
         project_dir="/home/azureuser/mohre-poc",
         profiles_dir="/home/azureuser/.dbt",
-        dbt_cli_profile=dbt_cli_profile,
-    ) as dbt_operation:
-        dbt_process = dbt_op.trigger()
-        # do other things before waiting for completion
-        dbt_process.wait_for_completion()
-        result = dbt_process.fetch_result()
+    ).run()
     return result
 
 trigger_dbt_flow()
